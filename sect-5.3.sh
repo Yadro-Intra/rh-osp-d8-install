c43e9484bb4e3b93a9a5c3208f0777bfd4b9a76b
jno <ed@ngslab.ru>
2016-09-14 15:28:22 +0300

    5.3 passed in somehow strange fascion

--- sect-5.3.sh
+++ sect-5.3.sh
@@ -34,10 +34,7 @@ cd ~stack || error $? "No home?"
 home=$(pwd)
 
 node_list () {
-	ironic node-list \
-	| tr -d '[ \t]' \
-	| cut -d\| -f2 \
-	| grep '^[a-f0-9]\+-....-....-....-[a-f0-9]\+$'
+	ironic node-list --fields uuid | grep -wo '[0-9a-f][0-9a-f-]\+'
 }
 
 assign_node_profile () {
@@ -45,15 +42,40 @@ assign_node_profile () {
 	local prf="$2"
 
 	run ironic node-update "$node" \
-		add properties/capabilities="profile:$prf,boot_option:local"
+		add properties/capabilities="profile:$prf"
 }
 
+# TFM says: 
+# Default profile flavors compute, control, swift-storage, ceph-storage, and block-storage
+# are created during Undercloud installation and are usable without modification in most
+# environments.
+
+nrules=$(openstack baremetal introspection rule list | grep -v '^[[:space:]]*$' | wc -l)
+(( nrules == 0 )) && echo "You have no baremetal introspection rules defined." >/dev/tty \
+		  || openstack baremetal introspection rule list
+
+openstack overcloud profiles list
+
+# I have no ceph-storage
+declare roles=(
+	'compute,boot_option:local'
+	'control,boot_option:local'
+	'swift-storage'
+	'block-storage'
+)
+
+declare -i ri=0 # role index
+
+# actually, I have 4 nodes and 4 roles, so...
+
 for node in $(node_list); do
-	echo "Looking at node '$node'...">&2
-	ironic node-show "$node"
-	assign_node_profile "$node" 'compute'
+	role=${roles[$ri]}
+	echo "Turning node '$node' into '$role' role...">&2
+	assign_node_profile "$node" "$role"
+	let ri+=1
 done
 
+openstack overcloud profiles list
 
 
 echo "You're done." >&2
