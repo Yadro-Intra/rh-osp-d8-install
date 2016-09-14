4907cfb4e76f69305680cd8c322c9cee224a471e
jno <ed@ngslab.ru>
2016-09-14 14:46:48 +0300

    5.2 passed somehow. One may use different flavours of node commissioning

--- sect-5.2.sh
+++ sect-5.2.sh
@@ -37,16 +37,27 @@ home=$(pwd)
 
 ironic node-list
 
-echo "Run 'sudo journalctl -l -u openstack-ironic-inspector -u openstack-ironic-inspector-dnsmasq -u openstack-ironic-conductor -f' in a next window now..." >&2
+bulk_introspection () {
+	echo "Run 'sudo journalctl -l -u openstack-ironic-inspector" \
+		"-u openstack-ironic-inspector-dnsmasq" \
+		"-u openstack-ironic-conductor -f' in a next window now..." >&2
 
-run openstack baremetal introspection bulk start && { echo "Done.">&2; exit 0; }
+	run openstack baremetal introspection bulk start && { echo "Done.">&2; exit 0; }
+}
 
 introspect_node () {
 	local uuid="$1"
 
 	ironic node-set-maintenance --reason '5.2 stage' "$uuid" true || return 1
 	echo "Node '$uuid' locked ok.">&2
-	run openstack baremetal introspection start "$uuid"
+	echo "Which method to use?" >/dev/tty
+	select r in ; do
+		case "$r" baremetal ironic in
+		baremetal) run openstack baremetal introspection start "$uuid"; break;;
+		ironic) run ironic node-set-provision-state "$uuid" inspect; break;;
+		esac
+	done
+	read -p 'Type <RETURN> to continue when done...'
 	ironic node-set-maintenance "$uuid" false && echo "Node '$uuid' unlocked ok.">&2
 }
 
@@ -57,12 +68,26 @@ node_list () {
 	| grep '^[a-f0-9]\+-....-....-....-[a-f0-9]\+$'
 }
 
-for node in $(node_list); do
-	echo "Looking at node '$node'...">&2
-	ironic node-show "$node"
-	introspect_node "$node" || { echo "Node locked.">&2; continue; }
+one_by_one_introspectoin () {
+	local node
+
+	for node in $(node_list); do
+		echo "Looking at node '$node'...">&2
+		ironic node-show "$node" \
+			--fields uuid driver_info provision_state last_error power_state
+		run introspect_node "$node" || { echo "Node locked.">&2; continue; }
+	done
+}
+
+select r in bulk one-by-one list-again; do
+	case "$r" in
+	bulk)	bulk_introspection; break;;
+	one-by-one)	one_by_one_introspectoin; break;;
+	list-again)	ironic node-list;;
+	esac
 done
 
+ironic node-list 
 echo "You're done." >&2
 echo >&2
 
